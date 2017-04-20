45d82ade3639816d13d4ad524864ef0409ef37df
Michael Lane <mikelane@gmail.com>
2017-04-19 20:07:27 -0700

    First commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+        'description': 'Hexapawan AI',
+        'author': 'Michael Lane',
+        'url': 'http://github.com/mikelane/hexapawn/',
+        'download_url': 'http://github.com/mikelane/',
+        'author_email': 'mikelane@gmail.com',
+        'version': '0.1',
+        'install_requires': ['nose'],
+        'packages': ['hexapawn'],
+        'scripts': [],
+        'name': 'hexapawn'
+}
+
+setup(**config)
+
